0922091b69db6f2bf7d5acf275b2a0ee80eec8c1
Asraful-code235 <asrafulislamcodes@gmail.com>
2025-05-21 19:40:23 +0600

    Update allowed regions

--- allowedOrigins.js
+++ allowedOrigins.js
@@ -2,5 +2,6 @@ const allowedOrigins = [
   "https://si3-backend.vercel.app",
   "https://www.si3.space",
   "https://si3-dashboard.vercel.app",
+  "https://backend.si3.space/",
 ];
 export default allowedOrigins;
